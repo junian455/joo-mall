4db202711e955aff733ad649d87a5c93c51ce9d3
hjj <1064821280@qq.com>
2022-03-27 11:48:58 +0800

    feat: 逆向生成仓库服务基础架构

--- joomall-ware/src/main/java/com/jaron/joomall/ware/service/impl/WareSkuServiceImpl.java
+++ joomall-ware/src/main/java/com/jaron/joomall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,16 +1,16 @@
 package com.jaron.joomall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.jaron.common.utils.PageUtils;
 import com.jaron.common.utils.Query;
-
 import com.jaron.joomall.ware.dao.WareSkuDao;
 import com.jaron.joomall.ware.entity.WareSkuEntity;
 import com.jaron.joomall.ware.service.WareSkuService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareSkuService")

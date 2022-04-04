f85393bf916b4e4cd5edb7d83854e64103b16a37
hjj <1064821280@qq.com>
2022-04-04 08:32:21 +0800

    feat: 优惠券关联类目和商品spu

--- joomall-coupon/src/main/java/com/jaron/joomall/coupon/controller/CouponController.java
+++ joomall-coupon/src/main/java/com/jaron/joomall/coupon/controller/CouponController.java
@@ -2,7 +2,10 @@ package com.jaron.joomall.coupon.controller;
 
 import java.util.Arrays;
 import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +18,6 @@ import com.jaron.common.utils.PageUtils;
 import com.jaron.common.utils.R;
 
 
-
 /**
  * 优惠券信息
  *
@@ -23,66 +25,98 @@ import com.jaron.common.utils.R;
  * @email jaron@qq.com
  * @date 2021-12-11 15:22:58
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
-public class CouponController {
+public class CouponController
+{
     @Autowired
     private CouponService couponService;
 
+    @Value("${coupon.user.name}")//从application.properties中获取//不要写user.name，他是环境里的变量
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+    @RequestMapping("/test")
+    public R test(){
+
+        return R.ok().put("name",name).put("age",age);
+    }
+    /**
+     * 获取会员的优惠券
+     *
+     * @param
+     * @return
+     * @author huangjunjian
+     **/
+    @RequestMapping("/getmembercoupon")
+    public R getMemberCoupon()
+    {
+        CouponEntity couponEntity = new CouponEntity();
+        couponEntity.setCouponName("满100减20");
+        
+        return R.ok().put("coupons", Arrays.asList(couponEntity));
+    }
+    
     /**
      * 列表
      */
     @RequestMapping("/list")
     // @RequiresPermissions("coupon:coupon:list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params)
+    {
         PageUtils page = couponService.queryPage(params);
-
+        
         return R.ok().put("page", page);
     }
-
-
+    
+    
     /**
      * 信息
      */
     @RequestMapping("/info/{id}")
     // @RequiresPermissions("coupon:coupon:info")
-    public R info(@PathVariable("id") Long id){
-		CouponEntity coupon = couponService.getById(id);
-
+    public R info(@PathVariable("id") Long id)
+    {
+        CouponEntity coupon = couponService.getById(id);
+        
         return R.ok().put("coupon", coupon);
     }
-
+    
     /**
      * 保存
      */
     @RequestMapping("/save")
     // @RequiresPermissions("coupon:coupon:save")
-    public R save(@RequestBody CouponEntity coupon){
-		couponService.save(coupon);
-
+    public R save(@RequestBody CouponEntity coupon)
+    {
+        couponService.save(coupon);
+        
         return R.ok();
     }
-
+    
     /**
      * 修改
      */
     @RequestMapping("/update")
     // @RequiresPermissions("coupon:coupon:update")
-    public R update(@RequestBody CouponEntity coupon){
-		couponService.updateById(coupon);
-
+    public R update(@RequestBody CouponEntity coupon)
+    {
+        couponService.updateById(coupon);
+        
         return R.ok();
     }
-
+    
     /**
      * 删除
      */
     @RequestMapping("/delete")
     // @RequiresPermissions("coupon:coupon:delete")
-    public R delete(@RequestBody Long[] ids){
-		couponService.removeByIds(Arrays.asList(ids));
-
+    public R delete(@RequestBody Long[] ids)
+    {
+        couponService.removeByIds(Arrays.asList(ids));
+        
         return R.ok();
     }
-
+    
 }
